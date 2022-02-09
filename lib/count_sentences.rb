301167bccb7a02f773e767ea3b11478148198977
Doris Morales <moralesdoris880@gmail.com>
2022-02-09 17:05:36 -0500

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    puts self
+    sentence=self.split(/[\S]+[(\?)||(\!)||(\.$)]/)
+    puts sentence
+    sentence=sentence.count 
   end
 end
+
+#[(\?)||(\!)||(\.$)]
+#[(?<!\.)+\.+(?!\.)+]
+#([(\.)||(\?)||(\!)]\s\.?)
+#/([(\.)||(\?)||(\!)]\s\.?)/
